8931f67cb139453e38c311858b1076bdfd95bebd
anhtai <ngotai1807@gmail.com>
2025-07-23 09:46:19 +0700

    sửa //

--- frontend/config/config.js
+++ frontend/config/config.js
@@ -1,4 +1,4 @@
 const apiUrl =
   window.location.hostname === "localhost"
     ? "http://localhost:6969"
-    : "https://project-remake.onrender.com/";
+    : "https://project-remake.onrender.com";
