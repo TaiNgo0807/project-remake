a55b6ac3a9145cef1aa4dfb30016e57c0304cd2b
anhtai <ngotai1807@gmail.com>
2026-04-10 14:12:53 +0700

    bannerload

--- frontend/banner.js
+++ frontend/banner.js
@@ -3,14 +3,20 @@ const API_BASE = `${apiUrl}/api/v1`;
 
 fetch(`${API_BASE}/banners`)
   .then((response) => response.json())
-  .then((banners) => {
-    if (banners.length === 0) {
+  .then((res) => {
+    // Nếu API trả về { data: [...] } thì lấy res.data, nếu trả về thẳng mảng thì lấy res
+    const bannerList = Array.isArray(res) ? res : res.data;
+
+    if (!bannerList || bannerList.length === 0) {
       slider.innerHTML = "<p>No banners available.</p>";
     } else {
-      banners.forEach((banner) => {
-        slider.innerHTML += `<div class="slide-item active">
-          <img class="slide-img" src="${banner.image}" alt="${banner.alt}" />
-        </div>`;
+      // Clear trước khi thêm để tránh trùng lặp nếu cần
+      slider.innerHTML = "";
+      bannerList.forEach((banner) => {
+        slider.innerHTML += `
+                <div class="slide-item active">
+                    <img class="slide-img" src="${banner.image}" alt="${banner.alt}" />
+                </div>`;
       });
     }
   })
