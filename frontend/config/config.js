f660dde6cad8a3717819d9fad5347b988e993c08
anhtai <ngotai1807@gmail.com>
2026-04-29 14:27:06 +0700

    sửa config

--- frontend/config/config.js
+++ frontend/config/config.js
@@ -1,7 +1,6 @@
-//config.js
+config.js;
 
-// const apiUrl =
-//   window.location.hostname === "localhost"
-//     ? "http://localhost:6969"
-//     : "https://project-remake.onrender.com";
-const apiUrl = "http://localhost:6969";
+const apiUrl =
+  window.location.hostname === "localhost"
+    ? "http://localhost:6969"
+    : "https://project-remake.onrender.com";
